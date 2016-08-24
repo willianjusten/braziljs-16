a9dc9f5b3f1872d7110e32d119b64c3411301f50
Willian Justen <willianjustenqui@gmail.com>
2016-08-24 02:57:09 -0300

    Add new slides

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,7 @@ Reveal.initialize({
     // Optional reveal.js plugins
     dependencies: [
         { src: '/js/vendor/classList.js', condition: function() { return !document.body.classList; } },
+        { src: 'js/vendor/highlight/highlight.js', async: true, condition: function() { return !!document.querySelector( 'pre code' ); }, callback: function() { hljs.initHighlightingOnLoad(); } },
         { src: 'js/vendor/notes/notes.js', async: true }
     ]
 });
@@ -18,15 +19,6 @@ function heartBeat() {
     document.getElementById('heart').classList.add('activated');
     var audio = new Audio('musics/heartbeat.mp3');
     audio.play();
-}
-
-var winampPlay = false;
-function winamp() {
-    if(!winampPlay) {
-        var audio = new Audio('musics/winamp.mp3');
-        audio.play();
-        winampPlay = true;
-    }
 }
 
 Reveal.addEventListener('winamp', winamp);
