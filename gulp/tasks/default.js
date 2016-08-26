20f5c35aa10489c306679e4fea39af3d25b1d269
Willian Justen <willianjustenqui@gmail.com>
2016-08-26 02:33:35 -0300

    Update 2016-08-26T05:29:05.109Z

--- gulp/tasks/default.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var gulp = require('gulp');
-
-// Default task
-module.exports = gulp.task('default', ['js', 'jade', 'notes', 'stylus', 'imagemin', 'music', 'video', 'watch', 'browser-sync', 'js']);
